9e267c6b860c3f46d7b7390ec87c96676265e71a
Gautam010 <85187336+Gautam010@users.noreply.github.com>
2021-08-20 17:29:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function clicked() {
+    window.open("https://rzp.io/l/h5uJJwxqy", "_blank");
+  }
+
+  window.onload=function(){
+      document.getElementById("year").innerHTML=new Date().getFullYear();
+  }
